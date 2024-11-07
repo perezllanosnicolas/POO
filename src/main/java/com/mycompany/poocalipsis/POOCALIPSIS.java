f80c622582b3088a9636fdfa89e273efb87814f9
perez <perez@NicolasPerez>
2024-11-07 16:12:55 +0100

    Mi aporte en el Hola mundo en el archi P2kfdojffsjfpofj

---
+++ src/main/java/com/mycompany/poocalipsis/POOCALIPSIS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poocalipsis;
+
+/**
+ *
+ * @author perez
+ */
+public class POOCALIPSIS {
+    
+    public int a;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
